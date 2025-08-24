22876723cdadfd5cf669fb635ad0497b44571661
Samridhi Gulati <gulatisamridhi02@gmail.com>
2025-08-25 00:59:10 +0530

    Update BASE_URL for deployed backend

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL ='http://localhost:5000'
+export const BASE_URL ='https://mern-project-f0f0.onrender.com';
 // process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
 // export const BASE_URL = 'https://mern-project-f0f0.onrender.com';
 export const PRODUCTS_URL = '/api/v1/products';
